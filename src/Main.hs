025423990aea6d06bb20109221b76eb08e5f6cbb
Jacob Errington <nictytan@gmail.com>
2015-04-11 23:55:05 -0400

    add sample testing code

---
+++ src/Main.hs
@@ -0,0 +1,15 @@
+module Main where
+
+import Graphics.UI.GLUT
+
+main :: IO ()
+main = do
+    (_progName, _args) <- getArgsAndInitialize
+    _window <- createWindow "Hello World"
+    displayCallback $= display
+    mainLoop
+
+display :: DisplayCallback
+display = do
+    clear [ ColorBuffer ]
+    flush
